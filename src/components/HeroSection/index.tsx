676818700e6e9082442f7ac907e8fad7f196675b
Giovanna Cunha <giovannakelli@gmail.com>
2023-01-06 21:58:22 -0300

    hero

---
+++ src/components/HeroSection/index.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Button } from "../Button";
+import {
+  ArrowForward,
+  ArrowRight,
+  HeroBg,
+  HeroBtnWrapper,
+  HeroContainer,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  VideoBg,
+} from "./styles";
+
+export const HeroSection = () => {
+  const [hover, setHover] = React.useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg
+          autoPlay
+          loop
+          muted
+          src={`video.mp4`}
+          itemType="video/mp4"
+        ></VideoBg>
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Virtual Banking Made Easy</HeroH1>
+        <HeroP>
+          Sign up for a new account today and receive $250 in credit towards
+          your
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to="signup"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary
+            dark
+          >
+            Get started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
